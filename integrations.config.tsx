c7899e54322c7c6299d05e328a4aa72b4adb930c
Tim Philipp <info@deepdive-ki.de>
2025-01-23 01:23:11 +0100

    further clean up

--- integrations.config.tsx
+++ integrations.config.tsx
@@ -1,40 +1,10 @@
 const integrations = {
-  isSanityEnabled: true,
-  isStripeEnabled: true,
   isOpenAIEnabled: true,
-  isMailchimpEnabled: true,
+  isMailchimpEnabled: false,
   isAuthEnabled: true,
 };
 
 const messages = {
-  sanity: (
-    <div style={{ whiteSpace: "pre-wrap" }}>
-      Sanity is not enabled. Follow the{" "}
-      <a
-        href="https://nextjstemplates.com/docs/enableintegration"
-        className="text-primary underline"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        documentation
-      </a>{" "}
-      to enable it.
-    </div>
-  ),
-  stripe: (
-    <div style={{ whiteSpace: "pre-wrap" }}>
-      Stripe is not enabled. Follow the{" "}
-      <a
-        href="https://nextjstemplates.com/docs/enableintegration"
-        className="text-primary underline"
-        target="_blank"
-        rel="noopener noreferrer"
-      >
-        documentation
-      </a>{" "}
-      to enable it.
-    </div>
-  ),
   opanAi: (
     <div style={{ whiteSpace: "pre-wrap" }}>
       OpenAI is not enabled. Follow the{" "}
--- next.config.js
+++ next.config.js
@@ -2,14 +2,8 @@
 const nextConfig = {
   images: {
     domains: ["localhost"], // Allow loading images from localhost
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "cdn.sanity.io", // Allow images from Sanity's CDN
-        port: "",
-      },
-    ],
-  },
+
+    },
   output: "standalone", // Optimize the build for standalone deployment
 };
 
