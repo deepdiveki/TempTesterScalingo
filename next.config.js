5d2cae3e95edfcf62e40f5736e226b432829df6b
Tim Philipp <info@deepdive-ki.de>
2025-01-23 17:13:35 +0100

    versions updates

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["localhost"], // Allow loading images from localhost
-
-    },
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "localhost",
+      },
+    ],
+  },
   output: "standalone", // Optimize the build for standalone deployment
 };
+
 
 module.exports = nextConfig;
